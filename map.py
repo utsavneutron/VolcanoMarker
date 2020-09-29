8250e73776ab88f5b2a12a2b9ad420cacb0320ef
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2020-09-29 09:21:35 -0500

    Final

--- map.py
+++ map.py
@@ -21,12 +21,21 @@ def color_change(elevation):
 
 map = folium.Map(location=[38.58, -84], zoom_start=8)
 
-fg = folium.FeatureGroup(name="My Map")
+fg = folium.FeatureGroup(name="Volcano")
 
 
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.Marker(location=[lt, ln],
                                popup=str(el)+"m", icon=folium.Icon(color=color_change(el))))
 
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(
+    data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+
+
 map.add_child(fg)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 map.save("map1.html")
